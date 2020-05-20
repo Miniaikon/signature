1202b1ff8d88aa04dba14f8a30fe8888a15ab86d
Miniaikon <skategrama@gmail.com>
2020-05-20 17:19:37 -0500

    Validaciones 4

--- resources/js/src/views/Home.vue
+++ resources/js/src/views/Home.vue
@@ -322,7 +322,7 @@ export default {
                 return item.NombreMedioPago == 'PAGO PENDIENTE';
             });
             console.log(paquete);
-            if(paquete !== undefined){
+            if(paquete === undefined){
                 alert('Tienes paquetes sin enviar');
             }else{
                 let me = this;
